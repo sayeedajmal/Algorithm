2377f02ef634eb4d525dc4008ae0bc412976bb8b
SAYEED AJMAL <sayeedajmala06@gmail.com>
2021-04-21 00:40:44 +0530

    added star pattern

--- JAVA/StarPattern.java
+++ JAVA/StarPattern.java
@@ -1,16 +1,9 @@
-;
-
 public class StarPattern {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.println("*");
-            }
-            System.out.println();
-        }
-        for (int i = 1; i <= 5; i++) {
-            for (int j = i; j <= 5; j++) {
-                System.out.println("*");
+        int i, j;
+        for (i = 1; i <= 5; i++) {
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
